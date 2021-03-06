afc27171a5af04ea7d918a1727a095a4a299ac6f
Alec Levin <alec@arlevin.org>
2021-03-05 20:30:23 -0500

    repackage

--- src/session/Session.js
+++ src/session/Session.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Axios from 'axios'
-import WebSocketClient from '../WebSocketClient'
+import WebSocketClient from './WebSocketClient'
 
 export default function Session() {
 
